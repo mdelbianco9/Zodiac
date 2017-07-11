5abd29a2c664336834c2ae79ff58a419aa44f43e
marcus delbianco <mdelbianco9@gmail.com>
2017-07-11 11:18:09 -0700

    final js

--- js/main.js
+++ js/main.js
@@ -14,64 +14,76 @@ var sign = ["aries",
 			];
 // The characteristics of each sign in the form of an object with 5 names
 var array = [{
-	first: "Aries",
+	first: "ARIES",
+	brith: "March 21 - April 19",
 	attributes: "Honest, Courageous, Passionate, Optimistic, Determined",
-	image: ""
+	image: "img/aries.jpg"
 },
 {
-	first: "Taurus",
+	first: "TAURUS",
+	birth: "April 21 - May 20",
 	attributes: "Patient, Reliable, Devoted, Responsible, Stable",
-	image: ""
+	image: "img/taurus.png"
 },
 {
-	first: "Gemini",
+	first: "GEMINI",
+	birth: "May 21 - June 20",
 	attributes: "Gentle, Curious, Adaptable, Affectionate, Learns Quickly",
-	image: ""
+	image: "img/gemini.jpg"
 },
 {
-	first: "Cancer",
+	first: "CANCER",
+	birth: "June 21 - July 22",
 	attributes: "Loyal, Emotional, Sympathetic, Tenacious, Persuastive",
-	image: ""
+	image: "img/cancer.jpg"
 },
 {
-	first: "Leo",
+	first: "LEO",
+	birth: "July 23 - August 22",
 	attributes: "Creative, Passionate, Generous, Warm-hearted, Cheerful",
-	image: ""
+	image: "img/leo.jpg",
 },
 {
-	first: "Virgo",
+	first: "VIRGO",
+	birth: "August 23 - September 22",
 	attributes: "Loyal, Analytical, Kind, Hardworking, Practical",
-	image: ""
+	image: "img/virgo.jpg"
 },
 {
-	first: "Libra",
+	first: "LIBRA",
+	birth: "September 23 - October 22",
 	attributes: "Cooperative, Social, Fair-minded, Diplomatic, Gracious",
-	image: ""
+	image: "img/libra.jpg"
 },
 {
-	first: "Scorpio",
+	first: "SCORPIO",
+	birth: "October 23 - November 21",
 	attributes: "Brave, Passionate, Recourceful, Stubborn, True friend",
-	image: ""
+	image: "img/scorpio.jpg"
 },
 {
-	first: "Sagittarius",
+	first: "SAGITTARIUS",
+	birth: "November 22 - December 21",
 	attributes: "Generous, Idealistic, Sense of Humor, Adventurous, Enthusiastic",
-	image: ""
+	image: "img/sagittarius.jpg"
 },
 {
-	first: "Caricorn",
+	first: "CAPRICORN",
+	birth: "December 22 - January 19",
 	attributes: "Disiplined, Responsible, Self-control, Independant, Leader",
-	image: ""
+	image: "img/capricorn.jpg"
 },
 {
-	first: "Aquarius",
+	first: "AQUARIUS",
+	birth: "January 20 - February 18",
 	attributes: "Humanitarian, Progressive, Original, Loving, Intellectual",
-	image: ""
+	image: "img/aquarius.jpg"
 },
 {
-	first: "Pisces",
+	first: "PISCES",
+	birth: "February 19 - March 20",
 	attributes: "Intuitive, Artistic, Gentle, Wise, Musical",
-	image: ""
+	image: "img/pisces.jpg"
 }
 ];
 
@@ -82,21 +94,32 @@ var array = [{
  	// "for loop" is working
  	// "myfunction" wont restart after it returns correct value
 function myfunction() {
-	// 
+	// Loops through the array to find the correct sign name 
 	for (var i = 0; i < array.length; i++){
  		// goes and gets value of entered text
  		var inputdata = document.getElementById("sign-input").value;
- // compares input text value to value of a sign name
- 	if (inputdata == array[i].first) {
+ 	// compares input text value to value of a sign name
+ 	if (inputdata.toUpperCase() === array[i].first) {
  		console.log("Pulling from array works");
  		// Displays Name of sign
  		document.getElementById("sign-name").innerHTML = array[i].first;
+ 		// Displays birthdates of each sign
+ 		document.getElementById("birth-dates").innerHTML = array[i].birth;
  		// Displays image
-
+ 		document.getElementById("sign-image").src = array[i].image;
  		// Displays Lits of Attributes
  		document.getElementById("sign-info").innerHTML = array[i].attributes;
- 	}
- 	}
+ 		// Allows the function to restart
+ 		return;
+ 	};
+ 	// If "if" statment fails this will be done
+ 	console.log("no matches found")
+ 	document.getElementById("sign-name").innerHTML = "Sorry, No Matches Found"
+ 	document.getElementById("birth-dates").innerHTML = "";
+ 	document.getElementById("sign-image").src = "";
+ 	document.getElementById("sign-info").innerHTML = "";
+
+ 	};
  };
  	
  
