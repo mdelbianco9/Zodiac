447c22b4d3e274cd1b09436088b456aec5e6b5be
marcus delbianco <mdelbianco9@gmail.com>
2017-07-15 14:38:57 -0700

    Zodiac final

--- js/main.js
+++ js/main.js
@@ -99,9 +99,6 @@ var array = [{
 }
 ];
 
-var month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-var day = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
-]
 
 // Triggers the function if the enter key is pressed
 var hello = document.getElementById('sign-input');
